9f26eaae3c3c912e645ce5429200112a039ddc57
Nil Karadag <nil.karadag@vestel.com.tr>
2018-06-06 16:05:31 +0300

    simplify app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 "use strict"
 const Trie = require("./lib/trie");
 let words = [ "hello", "dog", "hell","cat","a", "help","helps","helping"];
+
 // Construct trie
 let dictionary = new Trie();
 for (let i = 0; i < words.length ; i++)
 dictionary.addWord(words[i]);
-let isPresent = dictionary.isPresent("hel");
-isPresent ? console.log("hel is present in the trie.") : console.log("hel is not present in the trie.");
+
+//Check isPresent method
+console.log(dictionary.isPresent("hel")); // false
+console.log(dictionary.isPresent("dog")); // true
+
+//Check getAutoSuggestions method
 console.log("get auto suggestions for hel:");
-let suggestions = dictionary.getAutoSuggestions(dictionary.root,"hel");
-console.log("best match for %70 similarity: "+dictionary.filterSuggestions(suggestions, "hel", 70));
+let suggestions = dictionary.getAutoSuggestions(dictionary.root,"hel");  // ["hello", "hell", "help","helps","helping"]
+
+//Check filterSuggestions method
+console.log("best match for %70 similarity: "+dictionary.filterSuggestions(suggestions, "hel", 70)); // hell
